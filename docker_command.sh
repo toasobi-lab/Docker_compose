c7f44856918138c5d469bcffc7f8e6e63ec6fd7c
toasobi <yolo2022@ymail.ne.jp>
2025-02-10 19:52:25 +0900

    revison about docker commands sh

--- docker_command.sh
+++ docker_command.sh
@@ -1,10 +1,9 @@
-Summary of Commands
-Command	Description
-docker-compose down	Stop and remove containers and networks.
-docker-compose down --volumes	Stop and remove containers, networks, and volumes.
-docker image prune -f	Remove dangling images.
-docker system prune -a --volumes	Remove all unused Docker resources (full cleanup).
-docker ps	List running containers.
-docker ps -a	List all containers.
-docker volume ls	List all volumes.
-docker network ls	List all networks.
+Summary of Commands / docker Command Description
+docker-compose down	# Stop and remove containers and networks.
+docker-compose down --volumes	# Stop and remove containers, networks, and volumes.
+docker image prune -f	# Remove dangling images.
+docker system prune -a --volumes	# Remove all unused Docker resources (full cleanup).
+docker ps	# List running containers.
+docker ps -a	# List all containers.
+docker volume ls	# List all volumes.
+docker network ls	# List all networks.
